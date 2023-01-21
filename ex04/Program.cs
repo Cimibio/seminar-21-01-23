8682e9327e29819669faddc427b42f8c370599ce
AndreySh <shingaryovan@gmail.com>
2023-01-22 00:26:00 +1200

    Добавлено 4е задание - работа с массивом в цикле

---
+++ ex04/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задать массив из 8 элементов и вывести их на экран 
+Console.WriteLine("Сгенерирован массив из 8ми цифр");
+int[] array = new int[8];
+for (int i = 0; i <= 7; i++)
+{
+    array[i] = new Random().Next(1,100);
+}
+
+for (int i = 0; i <= 7; i++)
+{
+    Console.WriteLine("Элемент массива под номером: " + i + " равен: " + array[i]);
+}
