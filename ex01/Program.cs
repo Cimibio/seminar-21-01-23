482aa4e3c2ef9fff428d874d58e1babfc61e979f
AndreySh <shingaryovan@gmail.com>
2023-01-21 23:21:22 +1200

    добавлено первое задание - вычисление квадратов от 1 до N

---
+++ ex01/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine(Math.Pow(i,2));
+}
