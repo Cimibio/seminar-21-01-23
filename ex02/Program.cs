7cdd2eff4d082bdc5c2f28bd85eca7afbace3284
AndreySh <shingaryovan@gmail.com>
2023-01-21 23:39:00 +1200

    добавлено второе задание - арифметическая прогрессия

---
+++ ex02/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Сумма чисел от 1 до N
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int result = ((1 + N)*N) / 2;
+Console.WriteLine(result);
+
+for (int i = 1; i <= N; i++) //в качестве факультатива добавили расчет всех сумм чисел от 1 до N
+{
+    result = ((1 + i)*i) / 2;
+    Console.WriteLine(result); 
+}
