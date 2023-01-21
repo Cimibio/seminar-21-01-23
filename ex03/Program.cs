feb211d81a7ed078b89c7995eaa55b8c4316ca1e
AndreySh <shingaryovan@gmail.com>
2023-01-21 23:58:41 +1200

    вычисление количества цифр в числе

---
+++ ex03/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вычисление количества цифр в числе
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int copy = N;
+int i = 0;
+while (copy > 0) //считаем количество цифр в числе
+{
+    
+    copy = copy / 10;
+    i++;
+}
+Console.WriteLine("В числе " + N + " " + i + " цифр");
